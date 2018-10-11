2f3cafa218c8fd2d147e00e2f2fbf9d1de08c52e
CYR <aiden_cao@outlook.com>
2018-10-11 09:48:42 +0800

    完成评论内容的html解析

--- DataBaseHomeWork/DataBaseHomeWork.py
+++ DataBaseHomeWork/DataBaseHomeWork.py
@@ -1,2 +1,11 @@
-import requests
+from bs4 import BeautifulSoup as bs
+from urllib import request
 
+requrl = 'https://movie.douban.com/subject/26363254/comments' +'?' +'start=0' + '&limit=20' 
+resp = request.urlopen(requrl) 
+html_data = resp.read().decode('utf-8') 
+soup = bs(html_data, 'html.parser') 
+comment_div_lits = soup.find_all('div', class_='comment')
+
+for item in comment_div_lits:
+        print(item.find_all('span', class_='short')[0].get_text())
