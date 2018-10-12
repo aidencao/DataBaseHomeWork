42b9bb5e5f616394358af713ce63057c3d47a224
CYR <aiden_cao@outlook.com>
2018-10-12 12:46:07 +0800

    完成数据库连接

--- DataBaseHomeWork/DataBaseHomeWort_MongoDB.py
+++ DataBaseHomeWork/DataBaseHomeWort_MongoDB.py
@@ -1,19 +1,17 @@
 import pandas as pd
 import json
-from pymongo import MongoClient
+import pymongo
 
-def __init__(self,collection):
-        self.collection=collection
-        self.OpenDB()
-def OpenDB(self):
-        user='******'
-        passwd='******'
-        host='******'
-        port='******'
-        auth_db='******'
-        uri = "mongodb://"+user+":"+passwd+"@"+host+":"+port+"/"+auth_db+"?authMechanism=SCRAM-SHA-1"
-        self.con = MongoClient(uri, connect=False)
-        self.db=self.con['wangdong']
-        self.collection=self.db[self.collection]
-def closeDB(self):
-    self.con.close()
+DB_ip = '74.82.218.42'#数据库url
+DB_port = 27017#数据库端口
+DB_name = 'DatabaseHomework'#数据库名
+DB_collection = 'Comment'#集合名
+
+#连接
+client = pymongo.MongoClient(DB_ip,DB_port)
+db = client[DB_name]
+myCollect = db[DB_collection]
+
+
+# 存入数据库
+db[DB_collection].remove({'age':30})
