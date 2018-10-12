b988affd0015bd87adcc606e7cc5349c0d0e97bf
CYR <aiden_cao@outlook.com>
2018-10-12 11:40:58 +0800

    开始编写数据库

--- DataBaseHomeWork/DataBaseHomeWort_MongoDB.py
+++ DataBaseHomeWork/DataBaseHomeWort_MongoDB.py
@@ -1,1 +1,19 @@
+import pandas as pd
+import json
+from pymongo import MongoClient
 
+def __init__(self,collection):
+        self.collection=collection
+        self.OpenDB()
+def OpenDB(self):
+        user='******'
+        passwd='******'
+        host='******'
+        port='******'
+        auth_db='******'
+        uri = "mongodb://"+user+":"+passwd+"@"+host+":"+port+"/"+auth_db+"?authMechanism=SCRAM-SHA-1"
+        self.con = MongoClient(uri, connect=False)
+        self.db=self.con['wangdong']
+        self.collection=self.db[self.collection]
+def closeDB(self):
+    self.con.close()
