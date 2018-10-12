b46846527bf0fca0b29cf3463b24b74a31d85dd0
CYR <aiden_cao@outlook.com>
2018-10-12 11:32:31 +0800

    完成HTML的解析

--- DataBaseHomeWork/DataBaseHomeWork.py
+++ DataBaseHomeWork/DataBaseHomeWork.py
@@ -1,11 +1,18 @@
 from bs4 import BeautifulSoup as bs
 from urllib import request
 
-requrl = 'https://movie.douban.com/subject/26363254/comments' +'?' +'start=0' + '&limit=20' 
+requrl = 'https://movie.douban.com/subject/26985127/comments' +'?' +'start=0' + '&limit=20' 
 resp = request.urlopen(requrl) 
 html_data = resp.read().decode('utf-8') 
-soup = bs(html_data, 'html.parser') 
+soup = bs(html_data, 'html.parser')
+
+#获取到评论标签
 comment_div_lits = soup.find_all('div', class_='comment')
 
 for item in comment_div_lits:
-        print(item.find_all('span', class_='short')[0].get_text())
+    info=item.find_all('span', class_='comment-info')[0]#获取评论信息标签
+    username=info.a.get_text()#获取用户名
+    watched=info.find_all('span', class_='')[0].get_text()#获取用户是否已经看过
+    time=info.find_all('span', class_='comment-time')[0].get_text(strip=True)#获取评论时间
+    vote=item.find_all('span', class_='votes')[0].get_text()#获取有用数
+    comment=item.find_all('span', class_='short')[0].get_text()#获取正文
