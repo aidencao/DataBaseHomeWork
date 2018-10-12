b8ce752deb5f8780a807526e55f2f5fb4ff437bf
CYR <aiden_cao@outlook.com>
2018-10-12 14:11:06 +0800

    增加计数

--- DataBaseHomeWork/DataBaseHomeWork_Crawler.py
+++ DataBaseHomeWork/DataBaseHomeWork_Crawler.py
@@ -13,8 +13,9 @@ soup = bs(html_data, 'html.parser')
 comment_div_lits = soup.find_all('div', class_='comment')
 
 #生成字典结构
-data_dict = {'username' : [],'watched' : [],'time' : [],'vote' : [],'comment' : []}
-
+data_dict = {'count':[],'username' : [],'watched' : [],'time' : [],'vote' : [],'comment' : []}
+count = 1
+#解析html
 for item in comment_div_lits:
     info = item.find_all('span', class_='comment-info')[0]#获取评论信息标签
     username = info.a.get_text()#获取用户名
@@ -24,15 +25,17 @@ for item in comment_div_lits:
     comment = item.find_all('span', class_='short')[0].get_text()#获取正文
 
     #在字典中存入内容
+    data_dict['count'].append(count)
     data_dict['username'].append(username)
     data_dict['watched'].append(watched)
     data_dict['time'].append(time)
     data_dict['vote'].append(vote)
     data_dict['comment'].append(comment)
+    count = count + 1
 
 # 创建DataFrame
-columns = ['username', 'watched', 'time', 'vote', 'comment']
-index = [i for i in range(len(data_dict['username']))]
+columns = ['count','username', 'watched', 'time', 'vote', 'comment']
+index = [i for i in range(len(data_dict['count']))]
 data_frame = pd.DataFrame(data_dict, columns=columns, index=index)
 
 #将数据存入数据库
